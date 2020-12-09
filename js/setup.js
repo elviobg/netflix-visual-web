4d335fdf518c12e49990f2bf76ad0a43da0f43dc
Elvio <elviobg@gmail.com>
2020-12-09 12:58:21 -0300

    Adiciona a funcionalidade do carrossel

---
+++ js/setup.js
@@ -0,0 +1,24 @@
+var owl = $('.owl-carousel');
+
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:50,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    nav:false,
+    responsive:{
+        0:{items:1},
+        600:{items:3},
+        1000:{items:5},
+    }
+});
+
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
